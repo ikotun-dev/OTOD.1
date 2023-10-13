534e1bac79d8df60a1e4d0d2ff34cca6548cb208
ikotun-dev <danlogan2003@gmail.com>
2023-10-13 07:50:32 +0100

    Added image

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import cv2
 import tensorflow as tf
 
-#using pretrained model 
 model = tf.saved_model.load('ssd_mobilenet_v1_coco_2017_11_17/saved_model')
 
-# Preprocess the image
+
 img = cv2.imread('image.jpg')
 img = cv2.resize(img, (300, 300))
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-# Run the model on the image
 detections = model.predict(tf.expand_dims(img, axis=0))
 
 for detection in detections[0]:
